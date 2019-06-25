4e7320aae6cadea0715df5dc8d9126936388163e
AngYony <AngYony@163.com>
2019-06-25 11:15:33 +0800

    ef core 官方示例

--- efcore/src/Ay.EFCore.Work/ContosoUniversity/Pages/Students/Details.cshtml.cs
+++ efcore/src/Ay.EFCore.Work/ContosoUniversity/Pages/Students/Details.cshtml.cs
@@ -27,7 +27,11 @@ namespace ContosoUniversity.Pages.Students
                 return NotFound();
             }
 
-            Student = await _context.Student.FirstOrDefaultAsync(m => m.ID == id);
+            Student = await _context.Student
+            .Include(s=>s.Enrollments).ThenInclude(e=>e.Course)
+            //
+            .AsNoTracking()
+            .FirstOrDefaultAsync(m => m.ID == id);
 
             if (Student == null)
             {
