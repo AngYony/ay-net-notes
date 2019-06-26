2e7df8b47e259fd11be7f2006c33bfcdc672c212
AngYony <AngYony@163.com>
2019-06-26 20:12:20 +0800

    配置管理——Bind读取配置到C#实例

---
+++ aspnetcore/exsrc/jessetalk/GettingStartedWithASPNetCore/OptionsBindSample/MyClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionsBindSample
+{
+    public class MyClass
+    {
+        public int ClassNo{ get; set; }
+        public string ClassDesc{ get; set; }
+        public List<Student> Students{ get; set; }
+
+    }
+
+    public class Student{
+     public string Name{ get; set; }
+     public string Age{ get; set; }
+    }
+}
