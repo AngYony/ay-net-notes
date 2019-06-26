91000af8f0e32d3072e8ae0f69f735c8ac2bf9fe
AngYony <AngYony@163.com>
2019-06-26 20:01:39 +0800

    配置管理——JSON文件配置

---
+++ aspnetcore/exsrc/jessetalk/GettingStartedWithASPNetCore/JsonConfigSample/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+
+namespace JsonConfigSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+            .AddJsonFile("wy.json");
+
+            var configuration= builder.Build();
+
+            Console.WriteLine($"ClassNo:{ configuration["ClassNo"]}");
+            Console.WriteLine(configuration["Students:0:name"]);
+            Console.WriteLine(configuration["Students:1:name"]);
+            Console.WriteLine(configuration["Students:2:name"]);
+
+            Console.Read();
+        }
+    }
+}
