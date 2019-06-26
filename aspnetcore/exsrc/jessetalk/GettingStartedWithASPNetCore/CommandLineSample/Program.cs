986e1315741a82a832f3d9dc4722d7f9a16b3133
AngYony <AngYony@163.com>
2019-06-26 20:01:14 +0800

    配置管理——命令行配置

---
+++ aspnetcore/exsrc/jessetalk/GettingStartedWithASPNetCore/CommandLineSample/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+
+namespace CommandLineSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var settings = new Dictionary<string, string>{
+            { "name","smallz" },
+            {"age","18" }
+            };
+
+
+            var builder = new ConfigurationBuilder()
+            .AddInMemoryCollection(settings)
+            .AddCommandLine(args); //命令行的优先级高于内存集合
+
+            var configuration = builder.Build();
+
+            Console.WriteLine($"name:{configuration["name"]}");
+            Console.WriteLine($"age:{configuration["age"]}");
+
+            Console.ReadLine();
+        }
+    }
+}
