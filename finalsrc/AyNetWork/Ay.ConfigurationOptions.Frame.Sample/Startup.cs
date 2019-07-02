4713a7c45e2cb76b89ec6f443e50979a34c05d6a
AngYony <AngYony@163.com>
2019-07-02 20:13:10 +0800

    asp.net core 选项必要组件完善

--- finalsrc/AyNetWork/Ay.ConfigurationOptions.Frame.Sample/Startup.cs
+++ finalsrc/AyNetWork/Ay.ConfigurationOptions.Frame.Sample/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Ay.ConfigurationOptions.Frame.Sample.OptionsModel;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -12,10 +13,29 @@ namespace Ay.ConfigurationOptions.Frame.Sample
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<AyOption>(Configuration);
+
+            services.Configure<AyOption>(ay =>
+            {
+                ay.Option1 = "value1_from_delegate";
+                ay.Option2 = 444;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_2);
+
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,10 +46,12 @@ namespace Ay.ConfigurationOptions.Frame.Sample
             {
                 app.UseDeveloperExceptionPage();
             }
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync(config["AAA"]);
-            });
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync(config["AAA"]);
+            //});
+
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
