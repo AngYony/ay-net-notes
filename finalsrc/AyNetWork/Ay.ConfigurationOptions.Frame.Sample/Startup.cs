09e95f84606d4275f66ef3d3f474b79b32e4cb36
AngYony <AngYony@163.com>
2019-07-10 13:58:31 +0800

    asp.net core host 示例补充和总结

--- finalsrc/AyNetWork/Ay.ConfigurationOptions.Frame.Sample/Startup.cs
+++ finalsrc/AyNetWork/Ay.ConfigurationOptions.Frame.Sample/Startup.cs
@@ -23,8 +23,11 @@ namespace Ay.ConfigurationOptions.Frame.Sample
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //注意：该方法内如果需要使用IConfiguration，必须通过构造函数注入得到
+
             services.Configure<AyOption>(Configuration);
 
+          
             services.Configure<AyOption>(ay =>
             {
                 ay.Option1 = "value1_from_delegate";
@@ -42,6 +45,10 @@ namespace Ay.ConfigurationOptions.Frame.Sample
         public void Configure(
             IApplicationBuilder app, IHostingEnvironment env, IConfiguration config)
         {
+
+            //注意：该方法内如果要使用IConfiguration，最佳做法是，直接通过方法参数得到，而不是使用构造函数注入的IConfiguration
+            //因为当通过IConfiguration获取值时，不同生命期的服务直接通过构造函数注入的IConfiguration得到的值可以存着不一致的情况。
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
