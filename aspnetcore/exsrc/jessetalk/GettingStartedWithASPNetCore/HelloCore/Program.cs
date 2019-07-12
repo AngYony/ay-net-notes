d8805c44727bf594a4a47ea167853020573b2b63
AngYony <AngYony@163.com>
2019-07-12 15:07:46 +0800

    asp.net core 教程 示例

---
+++ aspnetcore/exsrc/jessetalk/GettingStartedWithASPNetCore/HelloCore/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace HelloCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration(config=> {
+                config.AddJsonFile("ay.json");
+                config.AddCommandLine(args); //右击项目-属性-调试
+            
+            })
+            //.UseUrls("http://localhost:5002")
+
+                .UseStartup<Startup>();
+    }
+}
---
+++ aspnetcore/exsrc/jessetalk/GettingStartedWithASPNetCore/HelloCore/Startup.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HelloCore
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,IConfiguration configuration,IApplicationLifetime applicationLifetime)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            applicationLifetime.ApplicationStarted.Register(()=> {
+                Console.WriteLine("Started");
+            
+            });
+
+            applicationLifetime.ApplicationStopped.Register(()=> {
+                Console.WriteLine("Stopped");
+
+            });
+
+            applicationLifetime.ApplicationStopping.Register(() => {
+                Console.WriteLine("Stopping");
+
+            });
+
+            app.Map("/task", taskapp => {
+
+                taskapp.Run(async context => {
+
+                   await context.Response.WriteAsync("this is a task"); 
+                });
+            });
+
+
+            app.Use( async (context, next) =>
+            {
+                await context.Response.WriteAsync("1: before start...");
+
+                await next.Invoke();
+            
+            });
+
+            app.Use(next=> {
+                return (context) => {
+                    context.Response.WriteAsync("2: in the middle of start...");
+
+                    return next(context);
+                };
+                
+            });
+
+
+
+
+
+
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("3:start....");
+                await context.Response.WriteAsync($"ContentRootPath:{env.ContentRootPath}");
+                await context.Response.WriteAsync($"WebRootPath:{env.WebRootPath}");
+
+                await context.Response.WriteAsync(configuration["ay"]);
+            });
+        }
+    }
+}
