0be159bd2db1a2f3015415a162faf407cf632e30
AngYony <AngYony@163.com>
2019-07-24 17:55:33 +0800

    IdentityServer4登录中心

---
+++ aspnetcore/exsrc/jessetalk/DistributedProjectCombat/IdentityServerCenter/Config.cs
@@ -0,0 +1,42 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityServerCenter
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetResources()
+        {
+            return new List<ApiResource> {
+                new ApiResource("api","my api")
+
+            };
+
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client()
+                {
+                    ClientId = "client",
+
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    ClientSecrets = {
+                        new Secret("secret".Sha256())
+                    },
+                    
+                    AllowedScopes = { "api" }
+                }
+            };
+
+
+        }
+    }
+}
