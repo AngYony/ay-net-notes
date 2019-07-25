ecd9ffee9ebd2ce7456df710026ddac8cae11fa5
AngYony <AngYony@163.com>
2019-07-25 15:50:06 +0800

    第三方ClientCredential模式调用

---
+++ aspnetcore/exsrc/jessetalk/DistributedProjectCombat/ThirdPartyDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http;
+using IdentityModel.Client;
+
+namespace ThirdPartyDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var diso = DiscoveryClient.GetAsync("http://localhost:5000").Result;
+            if (diso.IsError)
+            {
+                Console.WriteLine(diso.Error);
+            }
+
+            var tokenClient = new TokenClient(diso.TokenEndpoint, "client", "secret");
+            var tokenResponse = tokenClient.RequestClientCredentialsAsync("api").Result;
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+            }
+            else
+            {
+                Console.WriteLine(tokenResponse.Json);
+            }
+
+            var httpClient = new HttpClient();
+            httpClient.SetBearerToken(tokenResponse.AccessToken);
+            var response= httpClient.GetAsync("http://localhost:5001/api/values").Result;
+
+            if(response.IsSuccessStatusCode){
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
