1378022526280b77fc91b187acb924a033264a61
AngYony <AngYony@163.com>
2020-05-27 20:33:34 +0800

    添加LINQ中Lookup的示例代码

---
+++ csharp/csharp-src/LINQ_Sample/LookupSample.cs
@@ -0,0 +1,45 @@
+﻿using LINQ_Sample.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Sample
+{
+    public class LookupSample
+    {
+        public static void Test()
+        {
+            List<BT_Factor_Detail_AG_MBG> factorList = new List<BT_Factor_Detail_AG_MBG>{
+                new BT_Factor_Detail_AG_MBG(){ GtnTypeName="A", FactorId=1, FactorValue=10},
+                new BT_Factor_Detail_AG_MBG(){ GtnTypeName="C", FactorId=2, FactorValue=20},
+                new BT_Factor_Detail_AG_MBG(){ GtnTypeName="B", FactorId=3, FactorValue=30},
+                new BT_Factor_Detail_AG_MBG(){ GtnTypeName="A", FactorId=4, FactorValue=40},
+                new BT_Factor_Detail_AG_MBG(){ GtnTypeName="B", FactorId=5, FactorValue=50},
+                new BT_Factor_Detail_AG_MBG(){ GtnTypeName="D", FactorId=6, FactorValue=60},
+                new BT_Factor_Detail_AG_MBG(){ GtnTypeName="B", FactorId=7, FactorValue=70},
+                new BT_Factor_Detail_AG_MBG(){ GtnTypeName="D", FactorId=8, FactorValue=80},
+
+            };
+
+            var gtntype_factor = factorList.ToLookup(a => a.GtnTypeName);
+            //等同于下面的语句，原因是ILookup派生自IEnumerable<IGrouping<TKey, TElement>>
+            //gtntype_factor = factorList.GroupBy(a => a.GtnTypeName);
+
+            foreach (IGrouping<string, BT_Factor_Detail_AG_MBG> factors in gtntype_factor)
+            {
+                string gtntype = factors.Key;
+                Console.WriteLine("GTNType:" + gtntype);
+
+                foreach (BT_Factor_Detail_AG_MBG y in factors)
+                {
+                    Console.WriteLine("  FactorId:" + y.FactorId.ToString());
+
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ csharp/csharp-src/LINQ_Sample/Model/BT_Factor_Detail_AG_MBG.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Sample.Model
+{
+    public class BT_Factor_Detail_AG_MBG
+    {
+        public int FactorId { get; set; }
+        public string CustomerID { get; set; }
+        public string ProfitCenter { get; set; }
+        public string APCCode { get; set; }
+        public string ProductNo { get; set; }
+        public string GtnTypeName { get; set; }
+        public decimal FactorValue { get; set; }
+        public byte Unit { get; set; }
+
+        public int StartMonth { get; set; }
+    }
+}
