9792f017f35e4937da39c1e495847c187df1383a
AngYony <AngYony@163.com>
2020-10-10 17:52:57 +0800

    深入浅出asp.net core-静态文件中间件

---
+++ aspnetcore/exsrc/books/SRQC_AspNetCore/S8_WebApplication/Startup.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace S8_WebApplication
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
+        {
+            if (env.IsDevelopment())
+            {
+                //如果存在异常并且环境是Development，此中间件会被调用，显示开发异常页面。
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseStaticFiles();
+
+            app.Run(async (context)=>{
+                context.Response.ContentType = "text/plain;charset=utf-8";
+                await context.Response.WriteAsync("你好，.net core！");
+            });
+
+
+            
+
+
+
+            //app.UseRouting();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});
+        }
+    }
+}
