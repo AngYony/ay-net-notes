c26fbc6fe68e8b81ee6d194b14a3806321eeb19e
AngYony <AngYony@163.com>
2020-10-16 15:32:49 +0800

    阶段二的2小节学习

---
+++ aspnetcore/exsrc/jgs/MyCamp/HostWebApplication/Program.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace HostWebApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+
+                    //ÅäÖÃipºÍ¶Ë¿Ú°ó¶¨
+                    webBuilder.UseKestrel(options => {
+                        options.ListenAnyIP(6000);
+                        //options.Listen()
+                        options.ListenLocalhost(6001, opts => opts.UseHttps());
+                    });
+                });
+    }
+}
