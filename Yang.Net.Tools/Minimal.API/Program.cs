a5ecee079edf9e9cfc7bb4184c515d9a19435dbc
AngYony <AngYony@163.com>
2022-03-19 12:54:36 +0800

    minimal Api示例

--- Yang.Net.Tools/Minimal.API/Program.cs
+++ Yang.Net.Tools/Minimal.API/Program.cs
@@ -1,10 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 // 默认就带有minimal api
+string Hi(string name) => $"Hi，{name}";
 
+//http://localhost:5068/mini?name=wy
 app.MapGet("/mini", (string name) => $"Hello {name}!");
-app.MapPost("/mini", () => "post方法");
-
-Func<string> a = () => { return "ss"; };
+//访问同上
+app.MapGet("/hi",Hi);
+//将name作为路径中的一种：http://localhost:5068/hi/wy
+app.MapGet("/hi/{name}", Hi);
+//带有参数的
+app.MapGet("/par/{username}/{age}", (string username, int age) => { return $"名称：{username}，年龄：{age}"; });
 
 app.Run();
