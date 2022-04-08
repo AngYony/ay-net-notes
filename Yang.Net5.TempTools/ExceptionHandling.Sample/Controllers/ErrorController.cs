26f77926809449035899bd6f3b8f87277f51ac41
AngYony <AngYony@163.com>
2022-04-08 18:34:55 +0800

    异常处理

---
+++ Yang.Net5.TempTools/ExceptionHandling.Sample/Controllers/ErrorController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling.Sample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ErrorController : ControllerBase
+    {
+         
+        [Route("/error")]
+        [HttpPost]
+        public IActionResult Error()
+        {
+            // 创建ProblemDetails响应
+            return Problem();
+        }
+
+       
+        [Route("/error-local-development")]
+        [HttpPost]
+        public IActionResult ErrorLocalDevelopment([FromServices] IWebHostEnvironment webHostEnvironment)
+        {
+            if (webHostEnvironment.EnvironmentName != "Development")
+            {
+                throw new InvalidOperationException(
+                    "非开发环境");
+            }
+            //从中间件中获取实现了该异常的方法
+            var context = HttpContext.Features.Get<IExceptionHandlerFeature>();
+            return Problem(
+                detail: context.Error.StackTrace,
+                title: context.Error.Message
+            );
+        }
+
+
+    }
+}
---
+++ Yang.Net5.TempTools/ExceptionHandling.Sample/HttpResponseExceptionFilter.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling.Sample
+{
+    public class HttpResponseExceptionFilter : IActionFilter, IOrderedFilter
+    {
+        public int Order => int.MaxValue - 10; //设置优先级
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            if (context.Exception is HttpResponseException exception)
+            {
+                context.Result = new ObjectResult(exception.Value)
+                {
+                    StatusCode = exception.Status,
+                };
+                //标注为已近被处理
+                context.ExceptionHandled = true;
+            }
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+
+        }
+    }
+}
