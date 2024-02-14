08e5069796de82b1e06f3b3856caf66aded51f15
angyony <AngYony@163.com>
2024-02-14 17:30:15 +0800

    样式及样式中的触发器

---
+++ dotnet-book-src/csharp_mnbj/WpfApp/S12_9/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace S12_9
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnxKey_Click(object sender, RoutedEventArgs e)
+        {
+            new StyleXKey().ShowDialog();
+        }
+
+        private void btnNoxKey_Click(object sender, RoutedEventArgs e)
+        {
+            new StyleNoXKey().ShowDialog();
+        }
+
+        private void btnStyleTrigger_Click(object sender, RoutedEventArgs e)
+        {
+            new StyleTrigger().ShowDialog();
+        }
+    }
+}
