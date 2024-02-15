24b486eb4b267ba96723230f0cb671f5b268984a
angyony <AngYony@163.com>
2024-02-15 12:27:11 +0800

    动画

---
+++ dotnet-book-src/csharp_mnbj/WpfApp/S12_11/StoryboardDemo.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace S12_11
+{
+    /// <summary>
+    /// StoryboardDemo.xaml 的交互逻辑
+    /// </summary>
+    public partial class StoryboardDemo : Window
+    {
+        System.Windows.Media.Animation.Storyboard storyboard = null;
+        public StoryboardDemo()
+        {
+            InitializeComponent();
+            storyboard = (System.Windows.Media.Animation.Storyboard)this.Resources["std"];
+        }
+
+        private void OnPlay(object sender, RoutedEventArgs e)
+        {
+            storyboard.Begin(); //开始播放
+        }
+
+        private void OnPause(object sender, RoutedEventArgs e)
+        {
+            storyboard.Pause(); //暂停播放
+        }
+
+        private void OnResume(object sender, RoutedEventArgs e)
+        {
+            storyboard.Resume(); //继续播放
+        }
+
+        private void OnStop(object sender, RoutedEventArgs e)
+        {
+            storyboard.Stop(); //停止播放
+        }
+    }
+}
